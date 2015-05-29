f6faf5bf99062fbd64d1c0ff760d376e2b82ef48
Chris Sefkin <c.sefkin@gmail.com>
2015-05-29 10:21:49 -0700

    pass both specs, full functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 var findReplace = function(sentence, findWord, replaceWord) {
-  var newSentence = sentence;
-  var pattern = /findWord/ig;
-  newSentence.replace( pattern, replaceWord );
-  return newSentence;
-
+  var re = new RegExp(findWord, "g");
+  var newSentence = sentence.replace(re, replaceWord)
+  return newSentence
 }
+
+
+
+// var pattern = /findWord/ig;
+// var finalSentence = sentence.replace(pattern, replaceWord);
+// return finalSentence
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,11 @@
 describe('findReplace', function() {
 
   it("will find and replace a word in a string", function() {
-    expect(findReplace("i love dogs", "dogs", "cats"));
+    expect(findReplace("i love dogs", "dogs", "cats")).to.eq("i love cats")
   });
+
+  it("will find and replace a word in a string", function() {
+    expect(findReplace("i love dogs dogs", "dogs", "cats")).to.eq("i love cats cats")
+  });
+
 });
